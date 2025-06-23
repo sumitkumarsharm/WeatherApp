1ca6985a67e9588db3e682e0fc9ebf9dfa060dc2
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-06-23 23:52:32 +0530

    chore: initialized Vite + React + Tailwind projec

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+const App = () => {
+  return (
+    <div
+      className={`w-full min-h-screen flex items-center justify-center bg-gradient-to-r   p-3 transition-colors duration-500`}
+    ></div>
+  );
+};
+
+export default App;
